a3442c5e6b301a0dd350255455af5d91592d6aea
Almendras2024 <pittietoon@gmail.com>
2024-08-13 01:45:01 -0400

    Add files via upload

--- conversiones.py
+++ conversiones.py
@@ -1,23 +1,24 @@
+# Ingresar tasas de conversión mediante sys.argv en el siguiente orden: Sol, Peso Argentino, Dólar Americano. 
+# Ejecucion: python conversiones.py 0.0046 0.093 0.0013 10000
+
+#Importacion de bibliotecas
 import sys
 
-def convertir_monedas(tasa_sol, tasa_peso_arg, tasa_dolar, valor_peso_chile):
-    # Convertir al valor  de las otras monedas en pesos chilenos 
-    valor_sol = valor_peso_chile * tasa_sol
-    valor_peso_arg = valor_peso_chile * tasa_peso_arg
-    valor_dolar = valor_peso_chile * tasa_dolar
-    
-    # Imprimir las respuestas
-    print(f"Los {valor_peso_chile} pesos equivalen a:")
-    print(f"{valor_sol} Soles")
-    print(f"{valor_peso_arg} Pesos Argentinos")
-    print(f"{valor_dolar} Dólares")
+#Consulta si los argumentos tienen la cantidad de datos necesarios
 
-if __name__ == "__main__":
-    # Obtener los argumentos desde la línea de comandos
-    tasa_sol = float(sys.argv[1])
-    tasa_peso_arg = float(sys.argv[2])
-    tasa_dolar = float(sys.argv[3])
-    valor_peso_chile = float(sys.argv[4])
-    
-    # Llamar a la función para la conversion de monedas
-    convertir_monedas(tasa_sol, tasa_peso_arg, tasa_dolar, valor_peso_chile)
+if len(sys.argv) != 5:
+    print("Ingrese de forma correcta la sentencia, por ejemplo: python conversiones.py <Tasa Sol> <Tasa Peso Argentino> <Tasa Dólar Americano> <valor de peso chileno a convertir>")
+else:
+    #asigna los argumentos ingresados
+    tasa_sol_peruano, tasa_peso_argentino, tasa_dolar_americano, valor_peso_chileno = map(float, sys.argv[1:])
+   
+    #realiza cálculos para la reconversión
+    sol_convertido = valor_peso_chileno * tasa_sol_peruano
+    peso_argentino_convertido = valor_peso_chileno * tasa_peso_argentino
+    dolar_americano_convertido = valor_peso_chileno * tasa_dolar_americano
+
+    #imprime los resultados obtenidos en la conversión
+    print(f"Los {sys.argv[4]} pesos equivalen a:")
+    print(f"{sol_convertido:.1f} Soles")
+    print(f"{peso_argentino_convertido:.1f} Pesos Argentinos")
+    print(f"{dolar_americano_convertido:.1f} Dólares")
